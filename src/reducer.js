8098d10bf8b2ac1005aea573063d80ad5f25dd44
Olav Fosse <fosseolav@gmail.com>
2020-08-21 12:50:50 +0200

    Pass first test

---
+++ src/reducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action)
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default counterReducer
---
+++ src/reducer.test.js
@@ -0,0 +1,18 @@
+import counterReducer from './reducer'
+
+describe('unicafe reducer', () => {
+  const initialState = {
+    good: 0,
+    ok: 0,
+    bad: 0
+  }
+
+  test('should return a proper initial state when called with undefined state', () => {
+    const action = {
+      type: 'DO_NOTHING'
+    }
+
+    const newState = counterReducer(undefined, action)
+    expect(newState).toEqual(initialState)
+  })
+})
