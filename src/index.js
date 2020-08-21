2da0cb74c122ab31d8e86b66b9aaa4f4051d6f1c
Olav Fosse <fosseolav@gmail.com>
2020-08-21 14:38:30 +0200

    Implement application

--- src/index.js
+++ src/index.js
@@ -30,15 +30,30 @@ const App = () => {
     })
   }
 
+  const totalVotes = () => store.getState().good + store.getState().bad + store.getState().ok
+  const positivePercentage = () => store.getState().good / totalVotes() * 100
+  const average = () => (store.getState().good - store.getState().bad) / totalVotes()
+  const isFeedbackGiven = () => store.getState().good || store.getState().ok || store.getState().bad
+
   return (
     <div>
+      <h2>Give feedback</h2>
       <button onClick={good}>good</button> 
       <button onClick={neutral}>neutral</button> 
       <button onClick={bad}>bad</button>
       <button onClick={resetStats}>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>neutral {store.getState().ok}</div>
-      <div>bad {store.getState().bad}</div>
+      <h2>statistics</h2>
+      {
+        isFeedbackGiven() ? (
+          <>
+            <div>good {store.getState().good}</div>
+            <div>neutral {store.getState().ok}</div>
+            <div>bad {store.getState().bad}</div>
+            <div>average {average()}</div>
+            <div>positive {positivePercentage()} %</div>
+          </>
+        ) : 'No feedback given'
+      }
     </div>
   )
 }
