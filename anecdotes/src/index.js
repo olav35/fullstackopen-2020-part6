6ee350092e6aae5927b03cafd8e1daf4796ca439
Olav Fosse <fosseolav@gmail.com>
2020-08-24 15:50:25 +0200

    Extract store to store.js

--- anecdotes/src/index.js
+++ anecdotes/src/index.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
 import { Provider } from 'react-redux'
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
+import store from './store'
 
-const store = createStore(reducer, composeWithDevTools())
 
 ReactDOM.render(
   <Provider store={store}>
