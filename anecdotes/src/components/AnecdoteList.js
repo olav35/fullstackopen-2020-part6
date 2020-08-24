ddfeb189cbd172749a1f439d53cd8db48fc0983e
Olav Fosse <fosseolav@gmail.com>
2020-08-24 18:08:55 +0200

    Implement notification

--- anecdotes/src/components/AnecdoteList.js
+++ anecdotes/src/components/AnecdoteList.js
@@ -5,7 +5,7 @@ import { useSelector, useDispatch } from 'react-redux'
 
 const AnecdoteList = () => {
   const dispatch = useDispatch()
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => state.anecdotes)
   const vote = (id) => dispatch(voteOn(id))
 
   return (
---
+++ anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,4 @@
+// I believe this is what was referred to as the message
+const reducer = (state = '', action) => action.type
+
+export default reducer
