d446c96685f0b425423cf0565546f3f1d858a423
Olav Fosse <fosseolav@gmail.com>
2020-08-29 22:36:40 +0200

    Connectify AnecdoteForm

--- anecdotes/src/components/AnecdoteForm.js
+++ anecdotes/src/components/AnecdoteForm.js
@@ -1,18 +1,16 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
 
-const AnecdoteForm = () => {
-  const dispatch = useDispatch()
-
+const AnecdoteForm = (props) => {
   const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
 
-    dispatch(createAnecdote(content))
-    dispatch(setNotification(`Created "${content}"`))
+    props.createAnecdote(content)
+    props.setNotification(`Created "${content}"`)
   }
 
   return (
@@ -23,4 +21,10 @@ const AnecdoteForm = () => {
   )
 }
 
-export default AnecdoteForm
+const mapDispatchToProps = {
+  createAnecdote,
+  setNotification
+}
+
+const ConnectedAnecdoteForm = connect(null, mapDispatchToProps)(AnecdoteForm)
+export default ConnectedAnecdoteForm
